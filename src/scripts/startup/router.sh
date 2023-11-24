aa85b9738447b509eeb1824e834b85658dffe86d
Jonathan Main <jonathan.main@gmx.de>
2023-11-24 15:26:29 +0100

    bugfix: missing src ip for receivers from router

--- src/scripts/startup/router.sh
+++ src/scripts/startup/router.sh
@@ -5,9 +5,10 @@ ip link add name switch_bridge type bridge
 ip link set dev switch_bridge up
 ip link set eth0 master switch_bridge
 ip link set eth1 master switch_bridge
+ip route del default
 ip route del 10.3.0.0/16
 ip route del 10.4.0.0/16
-ip route add 10.3.0.0/16 dev switch_bridge
-ip route add 10.4.0.0/16 dev switch_bridge
+ip route add 10.3.0.0/16 dev switch_bridge src 10.3.0.11
+ip route add 10.4.0.0/16 dev switch_bridge src 10.4.0.11
 tc qdisc add dev switch_bridge root handle 1: netem rate 50mbit
 /usr/sbin/sshd -D
